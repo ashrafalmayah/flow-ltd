4f999b520bc8c4472efb6d7797368958ff11b548
Ashraf Almayah <ashrafalmayah00@gmail.com>
2024-06-09 14:33:57 +0300

    add navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<header class="sticky top-0 w-full">
+    <nav class="relative mx-auto flex max-w-5xl items-center justify-center py-4">
+        <ul class="flex gap-4 text-primary">
+            <li class="hover:underline"><a href="#">الرئيسية</a></li>
+            <li class="hover:underline"><a href="#">رؤيتنا</a></li>
+            <li class="hover:underline"><a href="#">رسالتنا</a></li>
+            <li class="hover:underline"><a href="#">قيمنا</a></li>
+            <li class="hover:underline"><a href="#">انجازاتنا</a></li>
+        </ul>
+        <x-contact-us-button class="absolute left-0"/>
+    </nav>
+</header>
