22bb2d42844e2f48894b86733ccf9e16e5769a17
Ashraf Almayah <ashrafalmayah00@gmail.com>
2024-06-09 14:35:13 +0300

    add hero section

---
+++ resources/views/components/contact-us-button.blade.php
@@ -0,0 +1,1 @@
+<button {{ $attributes(['class' => 'rounded-lg bg-primary hover:brightness-125 px-8 py-2 text-white']) }}>تواصل معنا</button>
---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,11 @@
+<section class="grid sm:grid-cols-2 p-8 justify-items-end items-center gap-24">
+    <div class="space-y-4">
+        <img src="{{ asset('assets/images/logo.png') }}" alt="Logo">
+        <h1 class="text-xl font-semibold spacing-2 tracking-tight">شريكك الموثوق والاحترافي للخدمات المساندة</h1>
+        <p>شركة سعودية متخصصة في الأعمال والخدمات المساندة (الصيانة والنظافة والتشغيل)</p>
+        <x-contact-us-button />
+    </div>
+    <div>
+        <img class="w-64" src="{{ asset('assets/images/hero.png') }}" alt="">
+    </div>
+</section>
