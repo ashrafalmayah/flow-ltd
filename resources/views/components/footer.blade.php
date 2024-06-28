6afda56c8db729fdb5a6f134552b0a34dbed6db4
husam almiyah <husamalmiyah@gmail.com>
2024-06-28 17:07:40 +0300

    fix some styles

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="relative grid py-12 place-content-center overflow-hidden bg-primary text-center text-white" id="footer">
-    <img class="absolute top-1/2 -translate-y-1/2 -right-[10%] rtl:-left-[10%] rtl:right-auto w-72 bg-primary sm:w-[450px] select-none"
+    <img class="absolute hidden sm:block top-1/2 -translate-y-1/2 -right-[10%] rtl:-left-[10%] rtl:right-auto w-72 bg-primary sm:w-[450px] select-none"
         src="{{ asset('assets/images/icon.svg') }}" alt="">
     <div class="z-10 space-y-4 p-12">
         <h2 class="text-5xl font-bold sm:text-7xl">{{ __('Contact Us') }}</h2>
