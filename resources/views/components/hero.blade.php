b8627f5248abb8183b5b008d98c505d74c7c1aaf
husam almiyah <husamalmiyah@gmail.com>
2024-06-28 18:27:35 +0300

    fix hero background

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,7 +1,7 @@
 <section class="relative grid items-center justify-items-center gap-4 p-8 py-36 text-center text-white">
     <div class="absolute top-0 bottom-0 overflow-hidden break-out">
         <div class="absolute inset-0 -z-10 bg-black"></div>
-        <div class="absolute inset-0 -z-10 bg-[url('/assets/images/background.jpg')] opacity-30"></div>
+        <div class="absolute inset-0 -z-10 bg-[url('/assets/images/background.jpg')] bg-bottom opacity-30"></div>
         <img class="absolute left-0 -z-10 bottom-0 sm:-bottom-24 sm:w-96 select-none"
             src="{{ asset('assets/images/icon.svg') }}" alt="">
     </div>
