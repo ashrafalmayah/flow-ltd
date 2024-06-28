1c32f55f17a9a327cec74786b93156934382e4a8
husam almiyah <husamalmiyah@gmail.com>
2024-06-28 17:20:25 +0300

    add partners label

--- resources/views/components/partners.blade.php
+++ resources/views/components/partners.blade.php
@@ -1,12 +1,14 @@
-<section dir="ltr" x-data="{}" x-init="$nextTick(() => {
-    let ul = $refs.logos;
-    ul.insertAdjacentHTML('afterend', ul.outerHTML);
-    ul.nextSibling.setAttribute('aria-hidden', 'true');
-    ul.nextSibling.classList.remove('animate-infinite-scroll');
-    ul.nextSibling.classList.add('animate-infinite-scroll-delayed');
-})"
-    class="inline-flex select-none my-8 w-full flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+<section>
     <h2 class="text-center text-5xl">{{ __('Our Partners') }}</h2>
+    <div dir="ltr" x-data="{}" x-init="$nextTick(() => {
+        let ul = $refs.logos;
+        ul.insertAdjacentHTML('afterend', ul.outerHTML);
+        ul.nextSibling.setAttribute('aria-hidden', 'true');
+        ul.nextSibling.classList.remove('animate-infinite-scroll');
+        ul.nextSibling.classList.add('animate-infinite-scroll-delayed');
+    })"
+    class="inline-flex select-none my-8 w-full flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+
     <ul x-ref="logos"
         class="flex animate-infinite-scroll items-stretch justify-center md:justify-start [&_img]:h-32 [&_img]:max-w-none [&_li]:mx-8">
         <li>
@@ -31,4 +33,6 @@
             <img src="{{ asset('assets/images/partners/ثابت.png') }}" alt="Thabit" />
         </li>
     </ul>
+    </div>
+
 </section>
