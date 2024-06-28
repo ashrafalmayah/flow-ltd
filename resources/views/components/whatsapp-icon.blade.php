f331a3ce76d8b0afb0cb2267b1920599e9e9c994
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-06-28 13:29:10 +0000

    update social info

--- resources/views/components/whatsapp-icon.blade.php
+++ resources/views/components/whatsapp-icon.blade.php
@@ -1,4 +1,4 @@
-<a class="fixed bottom-8 sm:bottom-4 right-4 w-16 sm:w-12 z-50" href="https://wa.me/+966576213893" target="blank">
+<a class="fixed bottom-8 sm:bottom-4 right-4 w-16 sm:w-12 z-50" href="https://wa.me/+966590727730" target="blank">
     <img src="{{ asset('assets/images/WhatsApp_icon.webp') }}" alt="Whatsapp">
     {{-- <span class="absolute inset-[13px] -z-10 animate-ping bg-green-500 rounded-full"></span> --}}
 </a>
