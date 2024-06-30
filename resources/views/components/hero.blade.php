e9ed649a52cc6aaef631b0f714512f75b65526dd
Ashraf Almayah <ashrafalmayah00@gmail.com>
2024-06-30 11:32:07 +0300

    fix hero

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,7 +1,7 @@
 <section class="relative grid items-center justify-items-center gap-4 p-8 py-36 text-center text-white">
     <div class="absolute top-0 bottom-0 overflow-hidden break-out -z-10">
         <div class="absolute inset-0 bg-[url('/assets/images/background.jpg')] bg-bottom"></div>
-        <div class="absolute inset-0 bg-black opacity-30"></div>
+        <div class="absolute inset-0 bg-black opacity-70"></div>
         <img class="absolute left-0 bottom-0 md:-bottom-24 md:w-96 select-none"
             src="{{ asset('assets/images/icon.svg') }}" alt="">
     </div>
