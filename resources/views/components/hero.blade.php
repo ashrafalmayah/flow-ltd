1a72fe42719a33a35349f7df4d70e6d4834fee17
Ashraf Almayah <ashrafalmayah00@gmail.com>
2024-06-30 12:36:13 +0300

    use an image instead of a background for hero

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,17 +1,17 @@
 <section class="relative grid items-center justify-items-center gap-4 p-8 py-36 text-center text-white">
-    <div class="absolute top-0 bottom-0 overflow-hidden break-out -z-10">
-        <div class="absolute inset-0 bg-[url('/assets/images/background.jpg')] bg-bottom"></div>
+    <div class="break-out absolute bottom-0 top-0 -z-10 overflow-hidden">
+        <img class="h-full w-full object-cover object-bottom" src="{{ asset('assets/images/background.jpg') }}" />
         <div class="absolute inset-0 bg-black opacity-70"></div>
-        <img class="absolute left-0 bottom-0 md:-bottom-24 md:w-96 select-none"
+        <img class="absolute bottom-0 left-0 select-none md:-bottom-24 md:w-96"
             src="{{ asset('assets/images/icon.svg') }}" alt="">
     </div>
 
     <img class="w-[32rem] select-none" src="{{ asset('assets/images/logo3.png') }}" alt="Logo">
-    <h1 class="spacing-2 text-xl sm:text-3xl font-semibold tracking-tight">
+    <h1 class="spacing-2 text-xl font-semibold tracking-tight sm:text-3xl">
         {{ __('Your Trusted Partner for Support Services') }}</h1>
     <p>{{ __('A Saudi company specialized in business and support services (maintenance, cleaning, and operation)') }}
     </p>
 
-    <x-contact-us-button class="text-2xl " />
+    <x-contact-us-button class="text-2xl" />
 
 </section>
